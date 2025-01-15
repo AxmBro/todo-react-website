b83493f7b52cd25e555ade2f1e833a83dc20700d
AmBro <axmbro@gmail.com>
2025-01-15 23:05:33 +0100

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 import React, { useState } from "react";
 import "./App.css";
-import { Todo } from "./components/Todo";
-import { AddTodo } from "./components/AddTodo";
+import { TodoContainer } from "./components/Todo";
+import { Header } from "./components/Header";
 import { ListsContext } from "./contexts/ListsContext";
 
 function App() {
   const [lists, setLists] = useState(() => {
     const lists = localStorage.getItem("lists");
-    return lists ? JSON.parse(lists) : localStorage.setItem("lists", JSON.stringify([]));;
+    return lists
+      ? JSON.parse(lists)
+      : localStorage.setItem("lists", JSON.stringify([]));
   });
   return (
     <>
       <ListsContext.Provider value={{ lists, setLists }}>
-        <h1>ToDo</h1>
-        <br />
-        <AddTodo></AddTodo>
-        <br />
-        <h2>Lists:</h2>
-        {lists.map((list) => {
-          return (
-            <Todo key={list} title={"test"} localStoragePrefix={list}></Todo>
-          );
-        })}
+        <Header />
+        <main>
+          <TodoContainer />
+        </main>
       </ListsContext.Provider>
     </>
   );
