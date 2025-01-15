564d550c5d313bb61147f31f2ea792f6404da740
AmBro <axmbro@gmail.com>
2025-01-15 23:16:39 +0100

    refactor: set all todo components in one file

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState, useContext } from "react";
 import styles from "./Todo.module.css";
 import { ListsContext } from "../contexts/ListsContext";
-import { AddTodo } from "./AddTodo";
 
 function TodoContainer() {
   const { lists } = useContext(ListsContext);
@@ -139,6 +138,45 @@ function TodoTaskItem({ task, tasks, setTasks, index }) {
       </div>
       <button onClick={handleRemoveTask}>X</button>
     </div>
+  );
+}
+
+function AddTodo() {
+  const [input, setInput] = useState("");
+  const { lists, setLists } = useContext(ListsContext);
+
+  const addNewList = (event) => {
+    event.preventDefault();
+    if (!input) return;
+    setLists((prev) => {
+      const listId = lists.length ? lists[lists.length - 1].id + 1 : 1;
+      const newUpdatedList = { id: listId, name: input };
+      const updatedLists = [...prev, newUpdatedList];
+      localStorage.setItem("lists", JSON.stringify(updatedLists));
+      return updatedLists;
+    });
+    setInput("");
+  };
+
+  const handleInputChange = ({ target }) => {
+    setInput(target.value);
+  };
+
+  return (
+    <>
+      <div className={styles.addTodo}>
+        <form action="" onSubmit={addNewList}>
+          <input
+            placeholder="Add New Todo List"
+            type="text"
+            name="addNewTodo"
+            value={input}
+            onChange={handleInputChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </>
   );
 }
 
